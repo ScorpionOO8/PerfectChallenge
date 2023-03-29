7c594ced7f46423476eacdef048f3b51dd635c88
ScorpionOO8 <johnmerkowsky@gmail.com>
2023-03-29 16:59:06 -0400

    likert scale!

---
+++ src/client/components/LikertScale.tsx
@@ -0,0 +1,59 @@
+// src/components/LikertScale.tsx
+import React from 'react';
+import { HStack, Button, Image, Box, useBreakpointValue } from '@chakra-ui/react';
+
+import image1 from '../assets/LikScl1.png';
+import image2 from '../assets/LikScl2.png';
+import image3 from '../assets/LikScl3.png';
+import image4 from '../assets/LikScl4.png';
+import image5 from '../assets/LikScl5.png';
+
+interface LikertScaleProps {
+  min: number;
+  max: number;
+  step: number;
+  value: number;
+  onChange: (value: number) => void;
+}
+
+const LikertScale: React.FC<LikertScaleProps> = ({ min, max, step, value, onChange }) => {
+  const images = [image1, image2, image3, image4, image5];
+  const options = [];
+
+  const buttonSize = useBreakpointValue({
+    base: '37.5px',
+    sm: '56.25px',
+    md: '75px',
+    lg: '75px',
+    xl: '75px',
+  });
+
+  for (let i = min; i <= max; i += step) {
+    options.push(
+      <Button
+        key={i}
+        onClick={() => onChange(i)}
+        p={0}
+        width={buttonSize}
+        height={buttonSize}
+        border="none"
+        backgroundColor={'transparent'}
+      >
+        <Box
+          width="100%"
+          height="100%"
+          border="4px"
+          borderColor={i === value ? 'white' : 'transparent'}
+          borderRadius="10px"
+          overflow="hidden"
+        >
+          <Image src={images[i - 1]} alt={`Option ${i}`} boxSize="100%" />
+        </Box>
+      </Button>
+    );
+  }
+
+  return <HStack spacing={3} justifyContent="center">{options}</HStack>;
+};
+
+export default LikertScale;
--- src/client/pages/QuizPage.tsx
+++ src/client/pages/QuizPage.tsx
@@ -1,8 +1,9 @@
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { useHistory } from 'react-router-dom';
 import { Box, Button, Container, Heading, VStack, FormControl, FormLabel, Radio, RadioGroup } from '@chakra-ui/react';
 import { Question } from '../../Shared/interfaces/Question';
 import { questions } from '../utils/questions';
+import LikertScale from '../components/LikertScale';
 
 interface QuizFormProps {
   question: Question;
@@ -23,15 +24,25 @@ const QuizForm: React.FC<QuizFormProps> = ({ question, onAnswered, isFinalQuesti
     <Box>
       <FormControl as="fieldset">
         <FormLabel as="legend">{isFinalQuestion ? 'How many habits do you want to take on?' : question.text}</FormLabel>
-        <RadioGroup value={selectedAnswer} onChange={(value: string) => setSelectedAnswer(parseInt(value))}>
-          <VStack spacing={2}>
-            {[...Array(5)].map((_, index) => (
-              <Radio key={index} value={index + 1}>
-                {index + 1}
-              </Radio>
-            ))}
-          </VStack>
-        </RadioGroup>
+        {isFinalQuestion ? (
+          <RadioGroup value={selectedAnswer} onChange={(value: string) => setSelectedAnswer(parseInt(value))}>
+            <VStack spacing={2}>
+              {[...Array(5)].map((_, index) => (
+                <Radio key={index} value={index + 1}>
+                  {index + 1}
+                </Radio>
+              ))}
+            </VStack>
+          </RadioGroup>
+        ) : (
+          <LikertScale
+            min={1}
+            max={5}
+            step={1}
+            value={selectedAnswer || 1}
+            onChange={(value) => setSelectedAnswer(value)}
+          />
+        )}
         <Button
           colorScheme="blue"
           mt={4}
